0a9c7d60e99436976531b8a7fd86cc226033a01b
Strychi8 <tomas2000T@gmail.com>
2025-01-14 14:39:20 -0300

    Creacion de app gestionPedidos, herencia de plantillas, forms y envio de mensajes

---
+++ TiendaOnline/gestionPedidos/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Clientes(models.Model):
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50, verbose_name='La direccion')
+    email = models.EmailField(blank=True, null=True)
+    telefono = models.CharField(max_length=7)
+
+    def __str__(self):
+        return self.nombre
+
+class Articulos(models.Model):
+    nombre = models.CharField(max_length=30)
+    seccion = models.CharField(max_length=20)
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return 'NOMBRE = %s, SECCION = %s, PRECIO = %s' % (self.nombre,self.seccion,self.precio)
+
+class Pedidos(models.Model):
+    numero = models.IntegerField()
+    fecha = models.DateField()
+    entregado = models.BooleanField()
